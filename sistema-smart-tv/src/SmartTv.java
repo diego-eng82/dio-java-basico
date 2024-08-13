8b92e3ad214a6afcd59ff4a14552d381e28a3df6
diego-eng82 <diego.borges.siqueira@gmail.com>
2024-08-13 20:50:23 -0300

    smarTv

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,7 +1,29 @@
 public class SmartTv {
     boolean ligada=false;
-    int canal=1;
-    int volume=25;
+    int canal=2;
+    int volume=20;
+
+    public void mudarCanal(int novoCanal){
+        canal=novoCanal;
+    }
+    
+    public void aumentarCanal(){
+        canal++;
+    }
+    
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Volume: " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Volume: " + volume);
+    }
 
     public void ligar(){
         ligada=true;
